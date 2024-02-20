c07c9dece07b123c92d063242ff539efe59afd57
Gabriel <gabri.mevial@gmail.com>
2024-02-20 20:41:42 -0300

    requisito 24 e 25 prontos, erro corrigido

--- app/backend/src/sevices/LeaderBoardService.ts
+++ app/backend/src/sevices/LeaderBoardService.ts
@@ -1,7 +1,8 @@
 import MatchesModel from '../database/models/MatchesModel';
 import TeamsModel from '../database/models/TeamsModel';
 import { getTotalDraws, getTotalGames, getGoalsFavor, getGoalsOwn,
-  getTotalLosses, getTotalPoints, getTotalVictories,
+  getTotalLosses, getTotalPoints, getTotalVictories, getEfficiency,
+  getGoalsBalance, getTeamsByOrder,
 } from '../utils/leaderboardfunctions';
 // import ILeaderBoard from "../Interfaces/ILeaderBoard";
 // import { ServiceResponse } from "../Interfaces/ServiceResponse";
@@ -13,8 +14,8 @@ export default class LeaderBoardService {
   ) {}
 
   public async getAllLeaderBoards() {
-    const allMatches = await this.matchModel.findByProgress(false);
     const allTeams = await this.teamModel.findAll();
+    const allMatches = await this.matchModel.findByProgress(false);
     const leaderboard = allTeams.map((team) => ({
       name: team.teamName,
       totalPoints: getTotalPoints(team.id, allMatches),
@@ -24,7 +25,10 @@ export default class LeaderBoardService {
       totalLosses: getTotalLosses(team.id, allMatches),
       goalsFavor: getGoalsFavor(team.id, allMatches),
       goalsOwn: getGoalsOwn(team.id, allMatches),
+      goalsBalance: getGoalsBalance(team.id, allMatches),
+      efficiency: getEfficiency(team.id, allMatches),
     }));
+    getTeamsByOrder(leaderboard);
     return { status: 'SUCCESSFUL', data: leaderboard };
   }
 }
--- app/backend/src/utils/leaderboardfunctions.ts
+++ app/backend/src/utils/leaderboardfunctions.ts
@@ -1,6 +1,7 @@
+import ILeaderBoard from '../Interfaces/ILeaderBoard';
 import IMatch from '../Interfaces/IMatch';
 
-function getTotalVictories(id: number, matches: IMatch[]) {
+function getTotalVictories(id: number, matches: IMatch[]):number {
   const total = matches.filter((match) => id === match.homeTeamId
     && match.homeTeamGoals > match.awayTeamGoals).length;
   return total;
@@ -22,24 +23,20 @@ function getTotalLosses(id: number, matches: IMatch[]) {
   return total;
 }
 
-function getGoalsFavor(id: number, matches: IMatch[]) {
+function getGoalsFavor(id: number, matches: IMatch[]):number {
   const total = matches.reduce((acc, match) => {
     if (id === match.homeTeamId) {
       return acc + match.homeTeamGoals;
-    } if (id === match.awayTeamId) {
-      return acc + match.awayTeamGoals;
     }
     return acc;
   }, 0);
   return total;
 }
 
-function getGoalsOwn(id: number, matches: IMatch[]) {
+function getGoalsOwn(id: number, matches: IMatch[]):number {
   const total = matches.reduce((acc, match) => {
     if (id === match.homeTeamId) {
       return acc + match.awayTeamGoals;
-    } if (id === match.awayTeamId) {
-      return acc + match.homeTeamGoals;
     }
     return acc;
   }, 0);
@@ -48,8 +45,32 @@ function getGoalsOwn(id: number, matches: IMatch[]) {
 
 function getTotalGames(id: number, matches: IMatch[]) {
   const total = matches.filter((match) =>
-    id === match.homeTeamId || id === match.awayTeamId).length;
+    id === match.homeTeamId).length;
   return total;
+}
+
+function getGoalsBalance(id: number, matches: IMatch[]): number {
+  const goalsFavor = getGoalsFavor(id, matches);
+  const goalsOwn = getGoalsOwn(id, matches);
+
+  return goalsFavor - goalsOwn;
+}
+
+function getEfficiency(id: number, matches: IMatch[]): string {
+  const totalPoints = getTotalPoints(id, matches);
+  const totalGames = getTotalGames(id, matches);
+
+  const possiblePoints = totalGames * 3;
+  const efficiency = (totalPoints / possiblePoints) * 100;
+
+  return efficiency.toFixed(2);
+}
+
+function getTeamsByOrder(team: ILeaderBoard[]): ILeaderBoard[] {
+  team.sort((a, b) => b.totalPoints - a.totalPoints
+    || b.goalsBalance - a.goalsBalance
+    || b.goalsFavor - a.goalsFavor);
+  return team;
 }
 
 export {
@@ -60,4 +81,7 @@ export {
   getTotalLosses,
   getTotalPoints,
   getTotalVictories,
+  getGoalsBalance,
+  getEfficiency,
+  getTeamsByOrder,
 };
